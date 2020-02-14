c95fe850f83106695bb910e552b12d81507c7452
Roman Rieunier <rieunierroman@hotmail.fr>
2020-02-14 23:34:29 +0200

    Added frontend

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# PFE-Drug-Repositioning
-Enabling Computational Drug Repositioning. End of studies (ENSC / ENSEIRB) project for LaBRI.
+# Enabling Computational Drug Repositioning using Knowledge Graphs
+End of studies project conducted by Dr. Gayo DIALLO ([LaBRI](https://www.labri.fr/)) during the [ENSC](https://ensc.bordeaux-inp.fr/) / [ENSEIRB-MATMECA](https://enseirb-matmeca.bordeaux-inp.fr/) Artificial Intelligence course.
+
+![logo ensc](https://ensc.bordeaux-inp.fr/sites/default/files/logo-ensc.png)
+![logo enseirb](https://enseirb-matmeca.bordeaux-inp.fr/sites/default/files/logo-em.png) 
+
+This repository provides a visualization tool to help biochemists to find new purposes for drugs based on their links in knowledge graphs.
+
+**First of all, you should have a look to the respective READMEs of the backend and the frontend directories.**
+
+To run the project, open 2 terminal sessions. In the first one:
+```bash
+cd backend
+chmod +x server.sh
+./server.sh
+```
+
+In the second one:
+```bash
+cd frontend
+npm start
+```
---
+++ frontend/src/App.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Playground from "./Playground";
+import Autocomplete from "react-autocomplete";
+
+interface AppState {
+  search: string;
+  drugs: Array<string>;
+  nodes: object[];
+  links: object[];
+}
+
+class App extends React.Component<{}, AppState> {
+  text: string = "Lepirudin";
+  state: AppState = {
+    search: "Lepirudin",
+    drugs: [],
+    nodes: [],
+    links: []
+  };
+
+  constructor(props: object) {
+    super(props);
+    fetch("http://localhost:5000/drugs")
+      .then(data => data.json())
+      .then(data => this.setState({ drugs: data }));
+  }
+
+  render() {
+    const { drugs, search, nodes, links } = this.state;
+
+    const url = `http://localhost:5000/graph${search && "/" + search}`;
+    console.log(url);
+    return (
+      <div>
+        <h1>Drug repositioning</h1>
+        <p>Cédric MARTINEZ & Roman RIEUNIER</p>
+        <p>Gayo DIALLO (LaBRI)</p>
+        <br />
+        <label>
+          Molecule{" "}
+          <Autocomplete
+            getItemValue={item => item}
+            items={drugs}
+            renderItem={(item, isHighlighted) => (
+              <div
+                style={{ background: isHighlighted ? "lightgray" : "white" }}
+              >
+                {item.label}
+              </div>
+            )}
+            value={search}
+            onChange={e => this.setState({ search: e.target.value })}
+            onSelect={val =>
+              this.setState({ search: val }, () =>
+                fetch(url)
+                  .then(data => data.json())
+                  .then(({ nodes, links }) => this.setState({ nodes, links }))
+              )
+            }
+          />
+        </label>
+        <br />
+        <br />
+        <Playground
+          nodeDataArray={nodes}
+          linkDataArray={links}
+          updateGraph={() => {}}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
